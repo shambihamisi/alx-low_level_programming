b09c78ac9f6db378f196ec25a91111fab15fd1a6
shambihamisi <shambi.hamisi@gmail.com>
2024-07-01 06:30:13 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - function name
+ * @s: function parameter
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+int i;
+int count = o;
+
+for (i = 0; s[i] != '\0'; i++)
+	count++;
+return (count);
+}
